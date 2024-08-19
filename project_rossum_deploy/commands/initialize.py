6e8e38ff3a78e3e508cacb64b205e2b675189b76
Miroslav Řehounek <miroslav.rehounek@rossum.ai>
2024-08-19 09:37:42 +0200

    feat: Move non-secret information into prd_config.yaml file

--- project_rossum_deploy/commands/initialize.py
+++ project_rossum_deploy/commands/initialize.py
@@ -18,31 +18,41 @@ Also initializes it as a git repository.
 The user is then expected to provide .env credentials and download Rossum objects.
                """,
 )
-@click.argument(
-    "name",
-    default=".",
-)
-def init_project(name):
-    if os.path.exists(name) and name != ".":
-        print(Panel(f'Directory "{name}" already exists, skipping.'))
-        return
-
-    credentials_path = name + "/credentials.json"
-    if os.path.exists(credentials_path):
-        print(Panel(f'"{credentials_path}" already exists, skipping.'))
-        return
-
+@click.argument("name", default=".", type=click.Path(path_type=Path))
+def init_project(name: Path):
     if not os.path.exists(name) and name != ".":
         os.mkdir(name)
 
-        os.chdir(name)
-        subprocess.run(["git", "init"])
-        os.chdir("..")
+    # (re)initialize GIT
+    subprocess.run(["git", "init", name])
 
-    with open(name + "/.gitignore", "a") as wf:
-        wf.write("\ncredentials.json")
-    example_credentials_path = Path(__file__).parent.parent / "dummy_credentials.json"
+    git_ignore_path = name / ".gitignore"
+    credentials_ignore_line = "\ncredentials.json"
 
-    shutil.copyfile(example_credentials_path, credentials_path)
+    git_ignore_contents = git_ignore_path.read_text()
 
-    print(Panel(f'Initialized a new directory in "{name}" .'))
+    if credentials_ignore_line not in git_ignore_contents:
+        with open(name + "/.gitignore", "a") as wf:
+            wf.write(credentials_ignore_line)
+
+    credentials_path = name / "credentials.json"
+    copy_dummy_credentials_file(credentials_path)
+
+    config_path = name / "prd_config.yaml"
+    copy_dummy_config_file(config_path)
+
+    print(Panel(f'Initialized a new PRD directory in "{name}" .'))
+
+
+def copy_dummy_credentials_file(destination: Path):
+    if not os.path.exists(destination):
+        example_credentials_path = (
+            Path(__file__).parent.parent / "dummy_credentials.json"
+        )
+        shutil.copyfile(example_credentials_path, destination)
+
+
+def copy_dummy_config_file(destination: Path):
+    if not os.path.exists(destination):
+        example_config_path = Path(__file__).parent.parent / "dummy_config.yaml"
+        shutil.copyfile(example_config_path, destination)
