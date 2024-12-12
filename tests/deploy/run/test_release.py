142aba01374989ac7d814353517d714bb1e70b4c
Miroslav Řehounek <miroslav.rehounek@rossum.ai>
2024-12-12 20:39:10 +0100

    Write test guidelines for deploy command

---
+++ tests/deploy/run/test_release.py
@@ -0,0 +1,2 @@
+# Take a JSON object / path and apply the release file to it
+# Check the changes to be deployed or mock the API request and check after that
---
+++ tests/deploy/template/test_template.py
@@ -0,0 +1,2 @@
+# Test non-interactive mode: I pass in a file and it spits out an update (hooks, names, etc.)
+# This will test the interactive mode too for basic errors as they have the same functions, etc.
