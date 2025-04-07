a3ea91742fdf45f7d77ac4ee2822d9505c19df46
Václav Růt <vaclav.rut@rossum.ai>
2025-04-07 08:21:14 +0200

    Fix problem with forgotten import that was causing the instalation to fail.

--- deployment_manager/main.py
+++ deployment_manager/main.py
@@ -7,7 +7,6 @@ from deployment_manager.commands.download.download import (
     download_project_wrapper,
 )
 from deployment_manager.commands.initialize import init_project
-from deployment_manager.commands.migrate_mapping import migrate_mapping_wrapper
 from deployment_manager.commands.purge.purge import purge_object_types_wrapper
 from deployment_manager.commands.upload.upload import upload_project_wrapper
 
@@ -19,7 +18,6 @@ def main(): ...
 
 main.add_command(download_project_wrapper)
 main.add_command(init_project)
-main.add_command(migrate_mapping_wrapper)
 main.add_command(deploy)
 main.add_command(hook)
 main.add_command(purge_object_types_wrapper)
