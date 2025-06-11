4feecfffc5c3669a98e54f635e7eb454d3bbd267
jozef.krkoska <jozef.krkoska@rossum.ai>
2025-06-11 11:10:14 +0200

    adding to gitignore

--- deployment_manager/commands/initialize.py
+++ deployment_manager/commands/initialize.py
@@ -34,6 +34,7 @@ async def init_project(name: Path):
         "\n**/credentials.json",
         f"\n**/{settings.CREDENTIALS_FILENAME}",
         f"\n**/{settings.DEFAULT_DEPLOY_SECRETS_PARENT}/",
+        f"\n**/**/{settings.SEPARATE_KEYS_FILE_NAME}",
     ]
 
     git_ignore_contents = (
